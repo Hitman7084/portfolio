f478f1ef45dbdc88ac1dd53d979d5d98ae5976d5
Himanshu Mall <62309305+Hitman7084@users.noreply.github.com>
2025-11-01 21:47:22 +0530

    tailwind config done

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import './globals.css';
 
 const HomePage = () => {
   return (
-    <div>
+    <div className='font-bold'>
       <h1>Welcome to My Portfolio</h1>
       <p>This is the home page.</p>
     </div>
